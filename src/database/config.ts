d44fc759c882c55ff02346a89c13b8cd946d07e8
Jorge <jorestuard@gmail.com>
2025-02-03 21:21:35 +0100

    project start and base configurations

---
+++ src/database/config.ts
@@ -0,0 +1,14 @@
+import { createClient } from "@libsql/client/.";
+
+import dotenv from "dotenv";
+dotenv.config();
+
+const dbUrl = process.env.TURSO_DATABASE_URL!;
+const dbAuthToken = process.env.TURSO_AUTH_TOKEN;
+
+const db = createClient({ 
+  url: dbUrl,
+  authToken: dbAuthToken
+});
+
+export default db;
